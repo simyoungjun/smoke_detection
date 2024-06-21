f7c7f1c3661eda35f3a31bb82ad0daf03ac146b0
dripcharacter <wsms8646@gmail.com>
2024-06-21 07:02:54 +0000

    kafka stream to video

--- kafka_cv2_consume_test.py
+++ kafka_cv2_consume_test.py
@@ -1,27 +1,119 @@
 import cv2
 import numpy as np
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
+import ffmpeg
+import time
 
 def consume_camera_stream(server_ip, server_port, topic_name, group_id):
+    consumer = KafkaConsumer(
+        f"{topic_name}",
+        bootstrap_servers=f"{server_ip}:{server_port}",
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        group_id=f"{group_id}",
+        api_version=(2, 6, 0)
+    )
+    
+    for message in consumer:
+        frame_bytes = message.value
+
+        np_arr = np.frombuffer(frame_bytes, dtype=np.uint8)
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        if frame is not None:
+            # cv2.imshow('video', frame)
+            cv2.imwrite('png.png', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            print("Failed to decode frame")
+
+    cv2.destroyAllWindows()
+
+def store_kafka_stream_as_video(server_ip, server_port, topic_name, group_id, o_width, o_height, fps=60, video_len=60, save_dir=".", save_name="test_video", width=None, height=None, p_num_offset_dict=None):
+    def make_img_from_msg(msg_value):
+        np_arr = np.frombuffer(msg_value, dtype=np.uint8)
+        img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        return img
+    
+    frame_list=list()
+
     consumer=KafkaConsumer(
             f"{topic_name}",
             bootstrap_servers=f"{server_ip}:{server_port}",
             auto_offset_reset="earliest",
             enable_auto_commit=True,
-            group_id=f"{group_id}"
+            group_id=f"{group_id}",
+            api_version=(2, 6, 0)
             )
     
-    for message in consumer:
-        frame_bytes=message.value
+    partitions = consumer.partitions_for_topic(f"{topic_name}")
+    if not partitions:
+        raise Exception(f"No partitions found for topic {topic_name}")
+    
+    topic_partitions = [TopicPartition(f"{topic_name}", p) for p in partitions]
 
-        np_arr=np.frombuffer(frame_bytes, dtype=np.uint8)
-        frame=cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    while not all(tp in consumer.assignment() for tp in topic_partitions):
+        print("Waiting for partition assignment...")
+        consumer.poll(0)
+        time.sleep(1)
 
-        cv2.imshow('video',frame)
+    if p_num_offset_dict != None:
+        for idx, offset in p_num_offset_dict.items():
+                consumer.seek(topic_partitions[idx], offset)
+    
+    current_offsets = {tp: consumer.position(tp) for tp in topic_partitions}
+    print("Current Offsets:", current_offsets)
 
-        if cv2.waitKey(1) & 0xFF==ord('q'):
-            break
-    cv2.destroyAllWindows()
+    while len(frame_list) < fps * video_len:
+        try:
+            frame_list.append(make_img_from_msg(next(consumer).value))
+        except StopIteration:
+            message = consumer.poll(timeout_ms=1000)
+            for _, messages in message.items():
+                for msg in messages:
+                    frame_list.append(make_img_from_msg(msg.value))
+                    if len(frame_list) >= fps * video_len:
+                        break
+                if len(frame_list) >= fps * video_len:
+                    break
+    
+    if len(frame_list) < fps * video_len:
+        for tp, offset in current_offsets.items():
+            consumer.seek(tp, offset)
+            return None
+    
+    video_path = f"{save_dir}/{save_name}.mp4"
+    
+    if width != None and height != None:
+        process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f'{o_width}x{o_height}', r=fps)
+        .filter('scale', width, height)
+        .output(video_path, pix_fmt='yuv420p', vcodec='libx264', r=fps)
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+        )
+    else:
+        process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo', pix_fmt='bgr24', s=f'{o_width}x{o_height}', r=fps)
+        .output(video_path, pix_fmt='yuv420p', vcodec='libx264', r=fps)
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+        )
+    
+    for frame in frame_list:
+        process.stdin.write(frame.astype(np.uint8).tobytes())
+    
+    process.stdin.close()
+    process.wait()
+    
+    print(f"Video saved as {video_path}")
+
+    
 
 if __name__=="__main__":
-    consume_camera_stream("piai_kafka.aiot.town", "9092", "TF-CAM-DOOR1", "webcam-group")
+    # consume_camera_stream("piai_kafka.aiot.town", "9092", "TF-CAM-DOOR2", "webcam-group2")
+    store_kafka_stream_as_video("piai_kafka.aiot.town", "9092", "TF-CAM-DOOR2", "webcam-group2", 640, 480, 120, 60, ".", "second_test", 1280, 1080)
