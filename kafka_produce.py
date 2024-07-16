e3c0a82bfb4bdc858ff9ae9055396221f270895f
dripcharacter <wsms8646@gmail.com>
2024-07-16 04:07:36 +0000

    cv2 fps set

--- kafka_produce.py
+++ kafka_produce.py
@@ -1,11 +1,12 @@
 import cv2
 from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 import time
-
+import numpy as np
+FPS = 30
+MAX_FPS = 30
 def get_last_offset(server_ip, server_port, topic_name):
     consumer = KafkaConsumer(bootstrap_servers=f"{server_ip}:{server_port}")
     partitions = consumer.partitions_for_topic(topic_name)
-    
     last_offset = 0
     if partitions is not None:
         for partition in partitions:
@@ -16,39 +17,33 @@ def get_last_offset(server_ip, server_port, topic_name):
             last_offset = max(last_offset, offset)
     consumer.close()
     return last_offset
-
 def produce_camera_stream(server_ip, server_port, topic_name):
     producer = KafkaProducer(bootstrap_servers=f"{server_ip}:{server_port}")
-
+    prev = 0
+    last_offset = get_last_offset(server_ip, server_port, topic_name)
     cap = cv2.VideoCapture(0)
-
+    cap.set(cv2.CAP_PROP_FPS, FPS)
+    print('width: {}, height : {}'.format(cap.get(3), cap.get(4)))
     if not cap.isOpened():
         print("Failed to open camera")
         return
-
+    frame_count = 0
+    start_time = time.time()
     while cap.isOpened():
         ret, frame = cap.read()
-        if not ret:
-            print("Failed to grab frame")
-            break
-
-        ret, buffer = cv2.imencode(".jpg", frame)
-        if not ret:
-            print("Failed to encode frame")
-            break
-
-        frame_bytes = buffer.tobytes()
-
-        # Get the last offset
-        last_offset = get_last_offset(server_ip, server_port, topic_name)
-        message_key = str(last_offset).encode('utf-8')
-
-        producer.send(topic_name, key=message_key, value=frame_bytes)
-
-        time.sleep(0.1)
-
+        if True:
+            prev = time.time()
+            if prev - start_time >= 1:
+                print(f"fps: {frame_count}")
+                frame_count = 0
+                start_time = prev
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame_bytes = buffer.tobytes()
+            last_offset += 1
+            message_key = str(last_offset).encode('utf-8')
+            producer.send(topic_name, key=message_key, value=frame_bytes)
+            frame_count += 1
     cap.release()
     producer.close()
-
 if __name__ == "__main__":
-    produce_camera_stream("piai_kafka.aiot.town", "9092", "TF-CAM-DOOR2")
+    produce_camera_stream("piai_kafka.aiot.town", "9092", "TF-CAM-TEST")
--- watchdog.sh
+++ watchdog.sh
@@ -5,14 +5,14 @@ echo "test success" >> /home/wsms8646/rpi4_kafka_test/result.txt
 _EXEC_PATH="kafka_produce.py"
 _EXEC_DIR="/home/wsms8646/rpi4_kafka_test/kafka_produce.py"
 function PID_CHECK() {
-        RUNNING_STAT=$(ps ax | grep $_EXEC_PATH | grep -v watchdog.sh | grep -v rotatelogs | grep -v grep | awk '{print $3}')
+        RUNNING_STAT=$(ps ax | grep $1 | grep -v watchdog.sh | grep -v rotatelogs | grep -v grep | awk '{print $3}')
 
         echo $RUNNING_STAT
         if [[ "$RUNNING_STAT" =~ ^[RS] ]]; then
                 echo "[$1] Process is running."
         else
                 echo "[$1] Process is stopped."
-                nohup python -u $_EXEC_DIR &
+                nohup python -u $2 &
                 # Wait for running.
                 sleep 3
         fi
